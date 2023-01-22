0aef7b69ac00fb12d423fdc5409598e9ac0ce107
Shrithesh1 <107168473+Shrithesh1@users.noreply.github.com>
2023-01-22 22:26:34 +0530

    Add files via upload

---
+++ Nature.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './snap.css'
+export default function Nature() {
+return (
+<div id='displaydiv'>
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..." />
+<img id='natureimage' src="image.jpg" class="rounded float"
+alt="..."></img>
+</div>
+)
+}
